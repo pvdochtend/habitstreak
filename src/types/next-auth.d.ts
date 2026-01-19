3c052cd6ad0dc0a930a4fb1a55c053f1a22a7c28
Peter vd Ochtend <pvdochtend@gmail.com>
2026-01-19 19:46:05 +0100

    feat(09-01): update type declarations for v5 compatibility
    - Import DefaultSession, DefaultUser from next-auth
    - Import DefaultJWT from next-auth/jwt
    - Extend default types instead of replacing
    - Session.user, User, and JWT types updated

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,22 +1,22 @@
-import 'next-auth'
-import 'next-auth/jwt'
+import { DefaultSession, DefaultUser } from 'next-auth'
+import { DefaultJWT } from 'next-auth/jwt'
 
 declare module 'next-auth' {
   interface Session {
     user: {
       id: string
       email: string
-    }
+    } & DefaultSession['user']
   }
 
-  interface User {
+  interface User extends DefaultUser {
     id: string
     email: string
   }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT {
+  interface JWT extends DefaultJWT {
     id: string
     email: string
   }
